3a46486972f3946c6b316cdf42f7fa4dd3443d3d
willandru <willandru@hotmail.com>
2022-05-10 16:37:33 -0500

    Varios archivos sql importantes, UPDATE, TRIGGER, GRANT ACCES

---
+++ TRIGGERS/updateBOOKS.sql
@@ -0,0 +1,7 @@
+UPDATE books
+set author_last_name='New title'
+where rating<4
+
+
+
+commmit;
---
+++ VERIFICAR_PERMISOS.sql
@@ -0,0 +1,4 @@
+SELECT owner, table_name, select_priv, insert_priv, delete_priv, update_priv, references_priv, alter_priv, index_priv 
+  FROM table_privileges
+ WHERE grantee = 'JCARRENO'
+ ORDER BY owner, table_name;
